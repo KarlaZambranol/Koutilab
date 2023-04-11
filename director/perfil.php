bdffaaaa312b34df859e26a5ef6b74fef8a3b0af
RafaelSoledad02 <wwrafasc1@gmail.com>
2023-04-11 13:30:20 -0600

    Reversion de cambios prueba

--- director/perfil.php
+++ director/perfil.php
@@ -7,7 +7,6 @@ if (empty($_SESSION['active'])) {
 ?>
 <!DOCTYPE html>
 <head> 
-  <link rel="stylesheet" href="">
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>KOUTILAB <?php echo $_SESSION['user'];?></title>
