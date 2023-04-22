edcf842f551fef21fbc29c08483075be3a677939
jaimereyesr <jaime.reyesr22@gmail.com>
2023-04-22 13:23:39 -0600

    Alertas de capsula premium

---
+++ alumno/secundaria/cursos/programacion-web/basico/capsulas/contenido/practicas/cp10html.php
@@ -0,0 +1,149 @@
+<?php
+session_start();
+$id_user = $_SESSION['idUser'];
+if (empty($_SESSION['active'])) {
+    header('location: ../../../../../../../../index.php');
+}
+include "../../../../../../../../acciones/conexion.php";
+$id_user = $_SESSION['idUser'];
+$permiso = "capsulapago2";
+$sql = mysqli_query($conexion, "SELECT c.*, d.* FROM capsulas_pago c INNER JOIN detalle_capsulas_pago d ON c.id_capsula_pago = d.id_permiso WHERE d.id_usuario = $id_user AND c.nombre = '$permiso' AND d.id_curso = 1;");
+$existe = mysqli_fetch_all($sql);
+if (empty($existe)) {
+    header("Location: ../../../../basico/capsulas/contenido/pasarela/capsula2html.php");
+}
+
+?>
+
+<!DOCTYPE html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>KOUTILAB</title>
+    <link rel="shortcut icon" href="../../../../../../img/lgk.png">
+    <link rel="stylesheet" href="../../css/capsula-practica.css">
+    <script src="https://kit.fontawesome.com/53845e078c.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+</head>
+
+<body>
+    <div class="body">
+        <div class="container">
+            <a href="../../../../../../rutas/ruta-pw-b.php"><button style="float: left;" class="btn-b" id="btn-cerrar-modalV"><i class="fas fa-reply"></i></button></a>
+            <div class="new-g" style="text-align: center;">Cápsula práctica 10 HTML</div><br>
+            <div class="board">
+                <table width="100%">
+                    <thead>
+                        <tr>
+                            <td>Instrucciones</td>
+                            <td>Ejemplo de resultado</td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td class="nombre">
+                                <p>Instrucciones: Crea un menú sencillo con algunas opciones
+                                    en forma de lista y un título del menú. Estas opciones no van a tener un link < ul> y < a>.
+                                            <br> <br>
+                                </p>
+                            </td>
+                            <td class="ne">
+                                <img src="../../../../../../img/menupractica.png">
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+
+            </div>
+            <div class="">
+                <h3>EDITOR DE CÓDIGO</h3>
+                <textarea onkeyup="actualizar()" class="cd" id="cd" placeholder="Escribe el código aquí"></textarea>
+                <iframe class="editor" id="editor" srcdoc=" "></iframe>
+            </div>
+            <a style="text-decoration: none;"><button onclick="miFunc()" type="submit" class="btn-grd" id="update" style="width: 20%; margin-top:1%;" disabled>Evaluar</button></a>
+
+        </div>
+    </div>
+    <script>
+        function miFunc() {
+            // checar que haya por lo menos 1 bold, italics y mark
+            var frame = document.getElementById("editor").contentWindow.document;
+            let ul = frame.querySelectorAll("ul").length;
+            let a = frame.querySelectorAll("a").length;
+
+            if (ul > 0 && a > 0) {
+                Swal.fire({
+                    title: '¡Bien hecho! ',
+                    text: '¡Puntuación guardada con éxito!',
+                    imageUrl: "../../../../../../img/Thumbs-Up.gif",
+                    imageHeight: 350,
+                    backdrop: `
+                    rgba(0,143,255,0.6)
+                    url("../../../../../../img/fondo.gif")
+                    `,
+                    confirmButtonColor: '#a14cd9',
+                    confirmButtonText: 'Aceptar',
+                }).then((result) => {
+                    window.location.href = '../../acciones/insertar_cp5.php?validar=' + 'correcto' + '&permiso=' + 5 + '&id_curso=' + 1 + '&practico=' + 10;
+                });
+            } else {
+                Swal.fire({
+                    title: 'Oops...',
+                    text: '¡Verifica tu respuesta!',
+                    imageUrl: "../../../../../../img/signo.gif",
+                    imageHeight: 350,
+                }).then((result) => {
+                    if (result.isConfirmed) {
+                        window.location.href = '../../acciones/insertar_cp5.php?validar=' + 'incorrecto' + '&permiso=' + 5 + '&id_curso=' + 1 + '&practico=' + 10;
+                    }
+                });
+            }
+        }
+    </script>
+    <script src="../../js/fund.js"></script>
+    <script>
+        function disableIE() {
+            if (document.all) {
+                return false;
+            }
+        }
+
+        function disableNS(e) {
+            if (document.layers || (document.getElementById && !document.all)) {
+                if (e.which == 2 || e.which == 3) {
+                    return false;
+                }
+            }
+        }
+        if (document.layers) {
+            document.captureEvents(Event.MOUSEDOWN);
+            document.onmousedown = disableNS;
+        } else {
+            document.onmouseup = disableNS;
+            document.oncontextmenu = disableIE;
+        }
+        document.oncontextmenu = new Function("return false");
+    </script>
+    <script>
+        onkeydown = e => {
+            let tecla = e.which || e.keyCode;
+
+            // Evaluar si se ha presionado la tecla Ctrl:
+            if (e.ctrlKey) {
+                // Evitar el comportamiento por defecto del nevagador:
+                e.preventDefault();
+                e.stopPropagation();
+
+                // Mostrar el resultado de la combinación de las teclas:
+                if (tecla === 85)
+                    console.log("Ha presionado las teclas Ctrl + U");
+
+                if (tecla === 83)
+                    console.log("Ha presionado las teclas Ctrl + S");
+            }
+        }
+    </script>
+</body>
